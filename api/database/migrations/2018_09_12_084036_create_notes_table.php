cbe4156488fee825a139f5f6ffe1f5f4446d01a8
B_Hordii <bhordiy@devtoday.com>
2018-11-13 16:58:07 +0200

    16:57.13-11.Test

--- api/database/migrations/2018_09_12_084036_create_notes_table.php
+++ api/database/migrations/2018_09_12_084036_create_notes_table.php
@@ -14,13 +14,13 @@ class CreateNotesTable extends Migration
      */
     public function up()
     {
-//        Schema::connection('mongodb')->create('notes', function (Blueprint $collection) {
-//            $collection->increments('id');
-//            $collection->string('caption')->unique();
-//            $collection->longText('text');
-//            $collection->string('parent_id')->nullable();
-//            $collection->timestamps();
-//        });
+        Schema::connection('mongodb')->create('notes', function (Blueprint $collection) {
+            $collection->increments('id');
+            $collection->string('caption')->unique();
+            $collection->longText('text');
+            $collection->string('parent_id')->nullable();
+            $collection->timestamps();
+        });
     }
 
     /**
@@ -30,9 +30,9 @@ class CreateNotesTable extends Migration
      */
     public function down()
     {
-//        Schema::connection('mongodb')->table('notes', function (Blueprint $collection) {
-//            $collection->dropIfExists();
-//        });
+        Schema::connection('mongodb')->table('notes', function (Blueprint $collection) {
+            $collection->dropIfExists();
+        });
 
             //Schema::dropIfExists('notes');
     }
--- api/tests/Feature/FolderTest.php
+++ api/tests/Feature/FolderTest.php
@@ -7,18 +7,58 @@ use Tests\TestCase;
 class FolderTest extends TestCase
 {
 
-    public function testCreate(){
-
-        $response = $this->post('/api/auth/folder/1',
+    public function test_Full_Create(){
+        $id = 1;
+        $response = $this->post('/api/auth/folder/'.$id,
             [
                 'title' => 'Folder_5',
-                'user_id' => '1',
             ]);
         var_dump($response->getContent());
         $this->assertEquals(201, $response->status());
     }
 
-    public function testUpdate()
+    public function test_Create_Successful(){
+        $id = 1;
+        $response = $this->post('/api/auth/folder/'.$id,
+            [
+                'title' => 'Folder_5',
+            ]);
+        var_dump($response->getContent());
+        $this->assertEquals(201, $response->status());
+    }
+
+    public function test_Create_EmptyTitle_Error(){
+
+        $id = 1;
+        $response = $this->post('/api/auth/folder/'.$id,
+            [
+                'title' => '',
+            ]);
+        var_dump($response->getContent());
+        $this->assertEquals(500, $response->status());
+    }
+    public function test_Create_AlreadyExist_Error(){
+
+        $id = 1;
+        $response = $this->post('/api/auth/folder/'.$id,
+            [
+                'title' => 'Folder_5',
+            ]);
+        var_dump($response->getContent());
+        $this->assertEquals(500, $response->status());
+    }
+    public function test_Create_WrongId_Error(){
+
+        $id ='';
+        $response = $this->post('/api/auth/folder/'.$id,
+            [
+                'title' => 'Folder_5',
+            ]);
+        var_dump($response->getContent());
+        $this->assertEquals(404, $response->status());
+    }
+
+    public function test_Update_Successful()
     {
         $response = $this->put('/api/auth/folder/1',
             [
@@ -28,9 +68,30 @@ class FolderTest extends TestCase
         var_dump($response->Content());
         $this->assertEquals(200, $response->status());
     }
+    public function test_Update_EmptyTitle_Error()
+    {
+        $response = $this->put('/api/auth/folder/1',
+            [
+                'title' => ''
+            ]);
 
-    public function testGet(){
-        $response = $this->get('/api/auth/folder/1',
+        var_dump($response->Content());
+        $this->assertEquals(500, $response->status());
+    }
+    public function test_Update_TitleExist_Error()
+    {
+        $response = $this->put('/api/auth/folder/1',
+            [
+                'title' => 'Upd_1 '
+            ]);
+
+        var_dump($response->Content());
+        $this->assertEquals(500, $response->status());
+    }
+
+    public function test_Get_Successful(){
+        $id=1;
+        $response = $this->get('/api/auth/folder/'.$id,
             [
 
             ]);
